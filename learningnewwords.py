55e72bc9f381a00c65a4d7bc758ada6090912e8a
Guzner0210 <43829885+Guzner0210@users.noreply.github.com>
2018-12-28 20:44:12 -0600

    Update learningnewwords.py

--- learningnewwords.py
+++ learningnewwords.py
@@ -1,6 +1,7 @@
 import sqlite3
+import random
 
-def main_board():
+def main():
     print("You have 2 options: \n")
     print("1.) Add a new word \n")
     print("2.) Take a quiz \n")
@@ -40,7 +41,46 @@ def new_word():
             print("Goodbye")
 
 def take_a_quiz():
-    pass
+    score = 0
+    number_of_question = 0
+    switc = True
+    while switc:
+        spawn_a_question()
+        your_answer_is = input("The word is: ")
+        if your_answer_is == the_answer_is:
+            score += 1
+            number_of_question += 1
+            print("That's correct!")
+        else:
+            number_of_question += 1
+            print("Sorry, you're wrong!")
+
+        iii = input("Do you want to keep continue? [Y/n] ").lower().strip()
+        if iii == "y" or iii == "yes":
+            continue
+        else:
+            break
+
+    print("Your score is: " + str((score/number_of_question)*100) +"%")
+
+
+def spawn_a_question():
+    db = sqlite3.connect("db'.sqlite3")
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM new_words')
+    all_rows = cursor.fetchall()
+    all_meanings = []
+    all_words = []
+    for row in all_rows:
+        index, word, meaning = row
+        all_meanings.append(meaning)
+        all_words.append(word)
+
+    a_rando = random.randint(0, len(all_meanings) - 1)
+    global the_answer_is
+    the_answer_is = all_words[a_rando]
+    print(all_meanings[a_rando])
+
 
 if __name__ == "__main__":
-    main_board()
+    main()
